0b9f95f795324c3291c926d83b80fcb980ef866a
ayushd701 <dixitayush0107@gmail.com>
2025-03-19 17:45:41 +0530

    Router and daisyui setup.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import {Routes , Route} from "react-router-dom"
+import {HomePage , ProfilePage , SettingsPage , SigninPage , SignupPage} from "./pages/index"
 
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className='text-white'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/signup' element={ <SignupPage /> } />
+        <Route path='/signin' element={ <SigninPage /> } />
+        <Route path='/profile' element={ <ProfilePage /> } />
+        <Route path='/settings' element={ <SettingsPage /> } />
+      </Routes>
+
     </div>
   )
 }
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import SignupPage from "./SignupPage"
+import SigninPage from "./SigninPage"
+import ProfilePage from "./ProfilePage"
+import SettingsPage from "./SettingsPage"
+
+export {HomePage , SettingsPage , SigninPage , ProfilePage , SignupPage}
