d03b1859023bba1fe840c90cc998ab0ccd2817e1
ayushd701 <dixitayush0107@gmail.com>
2025-03-19 13:00:48 +0530

    Auth routes and auth middleware created.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,11 +6,8 @@ import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
-    if(!email || !fullName || !password)
-    {
-        return res
-        .status(400)
-        .json({ message: "All fields required." });
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "All fields required." });
     }
     if (password.length < 6) {
       return res
@@ -42,10 +39,43 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password required." });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    generateToken(user._id, res);
+    return res.status(200).json({
+      id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    return res.status(500).json({error:"Internal server error"});
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("Logout route");
+  try {
+    res.cookie("jwt","",{maxAge:0});
+    return res.status(200).json({message:"Logged out successfully."})
+  } catch (error) {
+    console.log("Error in logout controller",error.message);
+    return res.status(500).json({message:"Internal server error."});
+  }
 };
+
+export const updateProfile = (req,res) => {
+  
+}
