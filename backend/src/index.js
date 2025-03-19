f4be896417ea8495b862b6e225a2ba84de134651
ayushd701 <dixitayush0107@gmail.com>
2025-03-19 15:24:17 +0530

    Message routes and controllers added.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import authRoute from "./routes/auth.route.js"
+import messageRoute from "./routes/message.route.js"
 import {connectMongoDb} from "./lib/db.js"
 
 dotenv.config();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoute);
+app.use("/api/message",messageRoute);
 
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`);
