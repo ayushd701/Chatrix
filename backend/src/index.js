9be218f2e1923dea1e20f15794f51178ff6619a7
ayushd701 <dixitayush0107@gmail.com>
2025-03-19 21:40:57 +0530

    Axios , authstore setup.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoute from "./routes/auth.route.js"
-import messageRoute from "./routes/message.route.js"
-import {connectMongoDb} from "./lib/db.js"
+import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
+import { connectMongoDb } from "./lib/db.js";
 
 dotenv.config();
-const app = express()
-const PORT = process.env.PORT 
+const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRoute);
-app.use("/api/message",messageRoute);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
 
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`);
-    connectMongoDb()
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectMongoDb();
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
-import {Routes , Route} from "react-router-dom"
+import {Routes , Route, Navigate} from "react-router-dom"
 import {HomePage , ProfilePage , SettingsPage , SigninPage , SignupPage} from "./pages/index"
+import { useAuthStore } from './store/useAuthStore'
+import {Loader} from "lucide-react"
 
 const App = () => {
+  const {authUser , checkAuth , isCheckingAuth} = useAuthStore();
+  useEffect(()=>{
+    checkAuth();
+  } , [checkAuth]);
+
+  console.log({authUser});
+
+  if( isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin'/>
+    </div>
+  )
+
   return (
     <div className='text-white'>
       <Navbar />
       <Routes>
-        <Route path='/' element={ <HomePage /> } />
-        <Route path='/signup' element={ <SignupPage /> } />
-        <Route path='/signin' element={ <SigninPage /> } />
-        <Route path='/profile' element={ <ProfilePage /> } />
-        <Route path='/settings' element={ <SettingsPage /> } />
+        <Route path='/' element={ authUser ? <HomePage /> : <Navigate to="/signin" /> } />
+        <Route path='/signup' element={ !authUser ? <SignupPage /> : <Navigate to="/" /> } />
+        <Route path='/signin' element={!authUser ? <SigninPage /> : <Navigate to="/" /> } />
+        <Route path='/profile' element={ authUser ? <ProfilePage /> : <Navigate to="/signin" /> } />
+        <Route path='/settings' element={ <SettingsPage />  } />
       </Routes>
 
     </div>
