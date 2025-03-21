944b2ed0320d06157821cb6fba3419fba5d484a9
ayushd701 <dixitayush0107@gmail.com>
2025-03-21 21:11:04 +0530

    Message input created and some correction done.

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -10,12 +10,12 @@ const ProfilePage = () => {
     const file = e.target.files[0];
     if(!file) return;
     const reader = new FileReader()
-    reader.readAsDataURL(file)
     reader.onload = async () => {
       const base64Image = reader.result;
       setSelectedImg(base64Image);
       await updateProfile({profilePic : base64Image})
     }
+    reader.readAsDataURL(file)
   }
 
   return (
